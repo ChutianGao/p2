eadba1cd8b7e9fa41a179edd3951bea35eb09e12
Charlie Gao <chg774@g.harvard.edu>
2018-02-11 17:04:47 -0500

    version 0.1

---
+++ bill-splitter-ctl.php
@@ -0,0 +1,113 @@
+<?php
+
+$total_charged = 0;
+$tips_rate = 0;
+$total = 0;
+$tips = 0;
+
+$number_people = 1;
+$charged_per_person = 0;
+$tips_per_person = 0;    
+$total_per_person = 0;
+
+
+$round_up = false;
+$round_up_checked = '';
+
+$selected_0 = '';
+$selected_10 = '';
+$selected_15 = '';
+$selected_18 = '';
+$selected_20 = '';
+
+$error_msg = array();
+
+// Assign Vars
+if (isset($_POST['submit']) && isset($_POST['submit']) == 'Calculate') {
+    foreach ($_POST as $key => $value) {
+        // Total Charged
+        if ($key == "total_charged") {
+            if (is_numeric($value)) {
+                $total_charged = floatval($value);
+            }
+        }
+        // Tips Rate
+        if ($key == "tips_rate") {
+            if (is_numeric($value)) {
+                $tips_rate = floatval($value) / 100;
+            }
+        }
+        // Number of People
+        if ($key == "number_people") {
+            if (is_numeric($value)) {
+                $number_people = intval($value);
+            }
+        }
+    }
+
+    // Round Up
+    if (isset($_POST['round_up'])) {
+        $round_up_checked = 'checked';
+        $round_up = true;
+    } else {
+        $round_up_checked = '';
+        $round_up = false;
+    }
+
+
+    // Validation
+    if ($total_charged < 0) {
+        $error_msg[] = 'Bill Total must be an positive integer.';
+    }
+
+    if ($tips_rate == 0 ) {
+        $error_msg[] = 'Please rate your satisfaction.';
+    }
+
+    if ($number_people <= 0) {
+        $error_msg[] = 'Number of People be an positive integer.';
+    }
+}
+
+// Config Display
+switch ($tips_rate) {
+    case 0.1:
+        $selected_10 = 'selected';
+        break;
+    case 0.15:
+        $selected_15 = 'selected';
+        break;
+    case 0.18:
+        $selected_18 = 'selected';
+        break;
+    case 0.2:
+        $selected_20 = 'selected';
+        break;    
+    default:
+         $selected_0 = 'selected';
+        break;
+}
+
+if (sizeof($error_msg) == 0) {
+    // Calculate
+    $total_charged = number_format($total_charged, 2, '.', '');
+    
+    $tips = $total_charged * $tips_rate;
+    $tips = number_format($tips, 2, '.', '');
+    
+    $tips_per_person = $tips / $number_people;
+    $tips_per_person = number_format($tips_per_person, 2, '.', '');
+
+    $total = $total_charged + $tips;
+    $total = number_format($total, 2, '.', '');
+
+    $charged_per_person = $total_charged / $number_people;
+    $charged_per_person = number_format($charged_per_person, 2, '.', '');
+
+    $total_per_person = $total / $number_people;
+    $total_per_person =  number_format($total_per_person, 2, '.', '');
+
+    if ($round_up) {
+        $total_per_person = round($total_per_person);
+    }
+}
---
+++ bill-splitter.php
@@ -0,0 +1,92 @@
+<?php
+require "bill-splitter-ctl.php";
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="Bill Splitter PHP App">
+    <meta name="author" content="Chutian Gao">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <title>Bill Splitter</title>
+    <!-- Bootstrap -->
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' type='text/css'>
+</head>
+<body>
+    <div class="container">      
+        <h2 class="text-center">Bill Splitter</h2>
+        <div class="row">
+            <div class="col-sm-offset-2 col-md-offset-2 col-lg-offset-3 col-xs-12 col-sm-8 col-md-8 col-lg-6">
+                <?php foreach($error_msg as $msg):?>
+                    <div class="alert alert-danger">
+                        <?= $msg;?>
+                    </div>
+                <?php endforeach;?>
+            </div>
+        </div>
+
+        <form action="" method="POST">
+        <div class="row">
+            <div class="col-sm-offset-2 col-md-offset-2 col-lg-offset-3 col-xs-12 col-sm-8 col-md-8 col-lg-6">
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-usd"></i></span>
+                    <input type="number" name="total_charged" class="form-control" placeholder="Total" value="<?= $total_charged; ?>" step="0.01">
+                </div>
+                <br>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                    <input type="number" name="number_people" class="form-control" placeholder="Numer of People" value="<?= $number_people; ?>">
+                </div>
+                <br>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-scale"></i></span>
+                    <select name="tips_rate" class="form-control" value="<?= $tips_rate; ?>">
+                        <option value="0" <?= $selected_0; ?> >Satisfaction</option>
+                        <option value="15" <?= $selected_15; ?> >Normal Lunch - 15%</option>
+                        <option value="18" <?= $selected_18; ?> >Normal Dinner - 18%</option>
+                        <option value="20" <?= $selected_20; ?> >Amazing - 20%</option>
+                        <option value="10" <?= $selected_10; ?> >Not Satisfied - 10%</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <p class="text-center">
+                <br><label class="checkbox-inline"><input type="checkbox" name="round_up" value="yes" <?= $round_up_checked; ?> >Round Up</label>
+                <br><br><input type="submit" class="btn btn-primary" name="submit" value="Calculate">
+            </p>
+        </div>
+        </form>
+
+        <div class="row">
+            <div class="col-sm-offset-2 col-md-offset-2 col-lg-offset-3 col-xs-12 col-sm-8 col-md-8 col-lg-6">
+                <div class="row">
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <div class="well">
+                            <p>
+                                <h4>All Together</h4>
+                                <label>Charged:</label> $<?= $total_charged; ?><br>
+                                <label>Tips:</label> $<?= $tips; ?><br>
+                                <label>Total:</label> $<?= $total; ?>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <div class="well">
+                            <p>                            
+                                <h4>Per Person</h4>
+                                <label>Charged:</label> $<?= $charged_per_person; ?><br>
+                                <label>Tips:</label> $<?= $tips_per_person; ?><br>
+                                <label>Owns:</label> $<u><?= $total_per_person; ?></u>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div><!-- /.container -->
+</body>
+</html>
